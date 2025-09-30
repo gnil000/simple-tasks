300b98b9cd9f02586e8971c53d540bdc26f84080
Elena Gnil <tony.vinz.one@gmail.com>
2025-09-30 20:04:21 +0300

    task4 new realization

--- go-core-task/4/main4_test.go
+++ go-core-task/4/main4_test.go
@@ -9,7 +9,7 @@ func TestLeftJoinSlices_Ok(t *testing.T) {
 	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
 	slice2 := []string{"banana", "date", "fig"}
 	expected := []string{"apple", "cherry", "43", "lead", "gno1"}
-	result := LeftJoinSlices(slice1, slice2)
+	result := leftJoinSlices(slice1, slice2)
 	if !slices.Equal(expected, result) {
 		t.Errorf("must be equal. expected %v, but got %v", expected, result)
 	}
@@ -19,7 +19,7 @@ func TestLeftJoinSlices_EmptyBothSlices(t *testing.T) {
 	slice1 := []string{}
 	slice2 := []string{}
 	expected := []string{}
-	result := LeftJoinSlices(slice1, slice2)
+	result := leftJoinSlices(slice1, slice2)
 	if !slices.Equal(expected, result) {
 		t.Errorf("must be equal. expected %v, but got %v", expected, result)
 	}
@@ -29,7 +29,7 @@ func TestLeftJoinSlices_EmptyFirstSlice(t *testing.T) {
 	slice1 := []string{}
 	slice2 := []string{"banana", "date", "fig"}
 	expected := []string{}
-	result := LeftJoinSlices(slice1, slice2)
+	result := leftJoinSlices(slice1, slice2)
 	if !slices.Equal(expected, result) {
 		t.Errorf("must be equal. expected %v, but got %v", expected, result)
 	}
@@ -39,7 +39,7 @@ func TestLeftJoinSlices_EmptySecondSlice(t *testing.T) {
 	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
 	slice2 := []string{}
 	expected := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
-	result := LeftJoinSlices(slice1, slice2)
+	result := leftJoinSlices(slice1, slice2)
 	if !slices.Equal(expected, result) {
 		t.Errorf("must be equal. expected %v, but got %v", expected, result)
 	}
--- go-core-task/4/main_4.go
+++ go-core-task/4/main_4.go
@@ -6,22 +6,38 @@ func main() {
 	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
 	slice2 := []string{"banana", "date", "fig"}
 	// result = {"apple", "cherry", "43", "lead", "gno1"}
-	fmt.Println(LeftJoinSlices(slice1, slice2))
+	fmt.Println(leftJoinSlices(slice1, slice2))
 }
 
-func LeftJoinSlices(slice1, slice2 []string) []string {
+// по умному с мапой
+func leftJoinSlices(slice1, slice2 []string) []string {
 	slice := make([]string, 0)
-	for _, v1 := range slice1 {
-		flag := true
-		for _, v2 := range slice2 {
-			if v1 == v2 {
-				flag = false
-				break
-			}
-		}
-		if flag {
-			slice = append(slice, v1)
+	setFromSlice2 := make(map[string]struct{})
+	for _, v := range slice2 {
+		setFromSlice2[v] = struct{}{}
+	}
+	for _, v := range slice1 {
+		if _, exists := setFromSlice2[v]; !exists {
+			slice = append(slice, v)
 		}
 	}
 	return slice
 }
+
+// реализация в тупую
+// func leftJoinSlices(slice1, slice2 []string) []string {
+// 	slice := make([]string, 0)
+// 	for _, v1 := range slice1 {
+// 		flag := true
+// 		for _, v2 := range slice2 {
+// 			if v1 == v2 {
+// 				flag = false
+// 				break
+// 			}
+// 		}
+// 		if flag {
+// 			slice = append(slice, v1)
+// 		}
+// 	}
+// 	return slice
+// }
