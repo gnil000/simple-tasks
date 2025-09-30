a3ffc5b9df3717ed5e0dff0022be094e1e242f61
Elena Gnil <tony.vinz.one@gmail.com>
2025-09-30 17:35:54 +0300

    first task completed

--- go-core-task/1/main1_test.go
+++ go-core-task/1/main1_test.go
@@ -1,1 +1,60 @@
-package main_test
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"slices"
+	"testing"
+)
+
+func TestCheckType(t *testing.T) {
+	if res := CheckType(42); res != "int" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	if res := CheckType(052); res != "int" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	if res := CheckType(0x2A); res != "int" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	var float32Num float64 = 53292.523
+	if res := CheckType(float32Num); res != "float64" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	if res := CheckType("hello"); res != "string" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	if res := CheckType(true); res != "bool" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+	var cmp complex64 = complex(5, 2)
+	if res := CheckType(cmp); res != "complex64" {
+		t.Errorf("expected be int, but got %s", res)
+	}
+}
+
+func TestCombineVariablesToString(t *testing.T) {
+	result := CombineVariablesToString(42, 0o52, 0x2A, 3.14, "hello", true, 5+2i)
+	expected := "42 42 42 3.14 hello true (5+2i)"
+	if result != expected {
+		t.Errorf("expected %q, but got %q", expected, result)
+	}
+}
+
+func TestConvertStringToSliceOfRune(t *testing.T) {
+	expectedRunes := []rune("hello")
+	if result := ConvertStringToSliceOfRune("hello"); !slices.Equal(expectedRunes, result) {
+		t.Errorf("expected %v, but got %v", expectedRunes, result)
+	}
+}
+
+func TestHashRunes(t *testing.T) {
+	textExemp := "my-text--end-of1"
+	hash := sha256.Sum256([]byte("my-text-go-2024-end-of1"))
+	expectedHashString := hex.EncodeToString(hash[:])
+
+	if result := HashRunes([]rune(textExemp)); expectedHashString != result {
+		t.Errorf("expected %s, but got %s", expectedHashString, result)
+	}
+
+}
--- go-core-task/1/main_1.go
+++ go-core-task/1/main_1.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var intNum10, intNum8, intNum16 int = 42, 052, 0x2A
+
+	fmt.Println(intNum10, intNum8, intNum16)
+
+	var float64Num float64 = 53292.523
+	fmt.Println(float64Num)
+
+	var stringSome string = "hello, world"
+	fmt.Println(stringSome)
+
+	var boolOp bool = true
+	fmt.Println(boolOp)
+
+	var complex64Num complex64 = complex64(6)
+	fmt.Println(complex64Num)
+
+	fmt.Println(CheckType(intNum10))
+	fmt.Println(CheckType(intNum8))
+	fmt.Println(CheckType(intNum16))
+	fmt.Println(CheckType(float64Num))
+	fmt.Println(CheckType(stringSome))
+	fmt.Println(CheckType(boolOp))
+	fmt.Println(CheckType(complex64Num))
+
+	combined := CombineVariablesToString(intNum10, intNum8, intNum16, float64Num, stringSome, boolOp, complex64Num)
+	fmt.Println(combined)
+
+	var runeOfSlice []rune = ConvertStringToSliceOfRune(combined)
+	hashed := HashRunes(runeOfSlice)
+	fmt.Println(hashed)
+}
+
+func CheckType(something any) string {
+	return reflect.TypeOf(something).String()
+}
+
+func CombineVariablesToString(intNum10, intNum8, intNum16 int, float64Num float64, stringSome string, boolOp bool, complex64Num complex64) string {
+	return fmt.Sprintf("%d %d %d %.2f %s %t %v", intNum10, intNum8, intNum16, float64Num, stringSome, boolOp, complex64Num)
+}
+
+func ConvertStringToSliceOfRune(text string) []rune {
+	return []rune(text)
+}
+
+func HashRunes(something []rune) string {
+	salt := []rune("go-2024")
+	mid := len(something) / 2
+	somethingSalt := append(something[:mid], append(salt, something[mid:]...)...)
+	hash := sha256.Sum256([]byte(string(somethingSalt)))
+	return hex.EncodeToString(hash[:])
+}
