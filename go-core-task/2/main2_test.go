d6f941de9582796388b74aba0ee44149bce29881
Elena Gnil <tony.vinz.one@gmail.com>
2025-09-30 18:47:09 +0300

    task2 completed

--- go-core-task/2/main2_test.go
+++ go-core-task/2/main2_test.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestCreateRandomSlice(t *testing.T) {
+	slice1 := createRandomSlice()
+	slice2 := createRandomSlice()
+	if slices.Equal(slice1, slice2) {
+		t.Error("expected different slices, but got similar")
+	}
+}
+
+func TestSliceExample(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expectedEvenSlice := []int{2, 4, 6, 8, 10}
+	resultSlice := sliceExample(slice)
+	if len(resultSlice) > len(slice) {
+		t.Errorf("length new slice (len = %d) cannot be more than original slice (len = %d)", len(resultSlice), len(slice))
+	}
+	if !slices.Equal(expectedEvenSlice, resultSlice) {
+		t.Errorf("expected %v, but got %v", expectedEvenSlice, resultSlice)
+	}
+}
+
+func TestAddElements(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expectedSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 112}
+	numForAdd := 112
+	result := addElements(slice, numForAdd)
+	if !slices.Equal(expectedSlice, result) {
+		t.Errorf("expected %v, but got %v", expectedSlice, result)
+	}
+}
+
+func TestCopySlice(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expectedSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	result := copySlice(slice)
+	if !slices.Equal(expectedSlice, result) {
+		t.Errorf("must be equal. expected %v, but got %v", expectedSlice, result)
+	}
+	result[0] = 666
+	if slices.Equal(expectedSlice, result) {
+		t.Errorf("shouldn't be equal. expected %v, but got %v", expectedSlice, result)
+	}
+}
+
+func TestRemoveElement(t *testing.T) {
+	slice1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	slice2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected1 := []int{2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected2 := []int{1, 2, 3, 4, 5, 7, 8, 9, 10}
+	result1 := removeElement(slice1, 0)
+	if !slices.Equal(expected1, result1) {
+		t.Errorf("must be equal. expected %v, but got %v", expected1, result1)
+	}
+	result2 := removeElement(slice2, 5)
+	if !slices.Equal(expected2, result2) {
+		t.Errorf("must be equal. expected %v, but got %v", expected2, result2)
+	}
+}
--- go-core-task/2/main_2.go
+++ go-core-task/2/main_2.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	originalSlice := createRandomSlice()
+	fmt.Println(originalSlice)
+
+	fmt.Println(sliceExample(originalSlice))
+
+	fmt.Println(addElements(originalSlice, 7))
+
+	copyS := copySlice(originalSlice)
+	copyS[0] = 111
+	fmt.Println(copyS, originalSlice) // изменения не повлияли
+
+	sliceRem := createRandomSlice()
+	fmt.Println(sliceRem, removeElement(sliceRem, 0))
+}
+
+func createRandomSlice() []int {
+	slice := make([]int, 0)
+	for range 10 {
+		slice = append(slice, rand.Intn(10))
+	}
+	return slice
+}
+
+func sliceExample(slice []int) []int {
+	resultSlice := make([]int, 0, cap(slice))
+	for _, v := range slice {
+		if v%2 == 0 {
+			resultSlice = append(resultSlice, v)
+		}
+	}
+	return resultSlice
+}
+
+func addElements(slice []int, num int) []int {
+	return append(slice, num)
+}
+
+func copySlice(slice []int) []int {
+	copyS := make([]int, len(slice))
+	copy(copyS, slice)
+	return copyS
+}
+
+func removeElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
 }
